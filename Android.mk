b93e272b19ffaae6435af180a35344cccaf8906f
Jackeagle <jackeagle102@gmail.com>
2022-10-24 06:40:20 +0200

    I001D: Mount Asus Firmware partition
    - Required as it contains OEM Speaker protection library
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),I001D)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+ASUSFW_MOUNT_POINT := $(TARGET_OUT_VENDOR)/asusfw
+$(ASUSFW_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(ASUSFW_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/asusfw
+
+ALL_DEFAULT_INSTALLED_MODULES += $(ASUSFW_MOUNT_POINT)
+
 endif
