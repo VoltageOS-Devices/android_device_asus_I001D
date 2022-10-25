cb5db113be761a8f48356b984a805e5fbf615913
Jackeagle <jackeagle102@gmail.com>
2022-10-25 11:59:37 +0200

    Blissify Asus ROG Phone 2
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_I001D.mk
+    $(LOCAL_DIR)/bliss_I001D.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_I001D-user \
-    lineage_I001D-userdebug \
-    lineage_I001D-eng
+    bliss_I001D-user \
+    bliss_I001D-userdebug \
+    bliss_I001D-eng
--- bliss_I001D.mk
+++ bliss_I001D.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from I001D device
 $(call inherit-product, device/asus/I001D/device.mk)
 
 PRODUCT_DEVICE := I001D
-PRODUCT_NAME := lineage_I001D
+PRODUCT_NAME := bliss_I001D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_I001D
 PRODUCT_MANUFACTURER := asus
