199f04dbdc735f4bca3a475f6a5bb0cfb8c40ea9
Jis G Jacob <86867971+StudioKeys@users.noreply.github.com>
2022-11-03 19:36:03 +0530

    Adapt for VoltageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_I001D.mk
+    $(LOCAL_DIR)/voltage_I001D.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_I001D-user \
-    bliss_I001D-userdebug \
-    bliss_I001D-eng
+    voltage_I001D-user \
+    voltage_I001D-userdebug \
+    voltage_I001D-eng
--- voltage_I001D.mk
+++ voltage_I001D.mk
@@ -9,16 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from I001D device
 $(call inherit-product, device/asus/I001D/device.mk)
 
 # Bootanimation
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1920
 
 PRODUCT_DEVICE := I001D
-PRODUCT_NAME := bliss_I001D
+PRODUCT_NAME := voltage_I001D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_I001D
 PRODUCT_MANUFACTURER := asus
@@ -29,3 +29,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="WW_I001D-user 11 RKQ1.200710.002 18.0210.2201.215-0 release-keys"
 
 BUILD_FINGERPRINT := asus/WW_I001D/ASUS_I001_1:11/RKQ1.200710.002/18.0210.2201.215-0:user/release-keys
+
+# UDFPS Animations
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
